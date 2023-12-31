789675b4477de2ca669da9a9d769b5a43ec074e6
WillHeather18 <s5418522@bournemouth.ac.uk>
2023-12-31 18:33:23 +0100

    test changes

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 const assert = require('assert');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
+function testIndexOf() {
+  const result = [1, 2, 3].indexOf(4);
+  try {
+    assert.equal(result, -1);
+    console.log('testIndexOf passed');
+  } catch (error) {
+    console.error('testIndexOf failed');
+  }
+}
+
+testIndexOf();
